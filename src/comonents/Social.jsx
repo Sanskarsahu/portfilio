3d69e32e9c062e41be81467443ee2da3dcb97aee
Sanskarsahu <85392227+Sanskarsahu@users.noreply.github.com>
2024-07-25 21:12:09 +0530

    Update Social.jsx

--- src/comonents/Social.jsx
+++ src/comonents/Social.jsx
@@ -5,23 +5,24 @@ export default function Social() {
     const social =[
         {
             icon:FaLinkedin,
+            link:"https://www.linkedin.com/in/sanskar-sahu-a8116a222/"
         },
         {
             icon:FaGithub,
+            link:"https://github.com/Sanskarsahu"
         },
         {
             icon:FaInstagram,
+            link:"https://www.instagram.com/"
         },
-        {
-            icon:FaWhatsapp
-        }
+        
 ]
   return (
     <motion.div animate={{opacity: 1}} initial={{opacity: 0}} transition={{duration:0.5 , delay:3}} className='flex flex-row space-x-2 '>
         {
             social.map((s)=>(
                 <div className='md:text-4xl text-3xl'>
-                    <s.icon/>
+                   <a href={s.link}><s.icon/></a>
                 </div>
             ))
         }
